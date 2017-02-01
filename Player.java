a9d24657cbdfef54d92c70709221736e7400b6aa
abain <abain@newroads.org>
2017-02-01 12:31:19 -0500

    ship

---
+++ Player.java
@@ -0,0 +1,37 @@
+
+public class Player{
+  //private String color;
+  private boolean win; 
+  private int ships; 
+  
+  public Player(){
+    /*color = "red";
+    suit = "diamonds";
+    rank = 'A';*/
+  }
+  public Player(boolean newWin, int newShips){
+    //color = newColor;
+    win = newWin;
+    ships = newShips;
+  }
+  
+  
+  public boolean getWin(){
+    return win;
+  }
+  public void setWin(boolean newWin){
+    win = newWin;
+  }
+  public int getShips(){
+    return ships;
+  }
+  public void setShips(char newShips){
+    ships = newShips;
+  }
+
+
+ public String toString(){
+    return win + " " + ships; 
+  }
+
+}
