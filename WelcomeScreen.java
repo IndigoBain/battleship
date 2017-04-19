9fd55b5fdb55f3895b705c4abf32f79f8e50cbfd
abain <abain@newroads.org>
2017-04-19 12:09:54 -0400

    battleship

---
+++ WelcomeScreen.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class WelcomeScreen {
+  
+    
+    public static void main String(){
+     
+System.out.println("Welcome to BattleShip!! Choose the placement of your first ship!!")
+ Scanner input = new Scanner(System.ln);
+      
+      
+      System.out.println()
+      
+    }
+
+ 
+     
